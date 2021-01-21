60791bfc2a1fe15380389b4fec56cf07d9e85f88
nkamuranodj <nkamuranodj@yahoo.co.jp>
2021-01-21 14:10:01 +0900

    デバイスによるuserモデル作成、テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        has_many :items, dependent:  :destroy
+        has_many :comments , dependent: :destroy
+        has_many :buyers
+        validates :nick_name, presence: true
+        validates :first_name, presence: true
+        validates :family_name, presence: true
+        validates :first_name_kana, presence: true
+        validates :family_name_kana, presence: true
+        validates :birth_day, presence: true
 end
