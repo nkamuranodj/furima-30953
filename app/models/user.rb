ff87671e02bfb2b9f6413cb9222c97706aae667d
nkamuranodj <nkamuranodj@yahoo.co.jp>
2021-01-23 16:25:17 +0900

    名前のバリデーション設定、ログアウトのパス記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        has_many :items, dependent:  :destroy
-        has_many :comments , dependent: :destroy
-        has_many :buyers
-        validates :nick_name, presence: true
-        validates :first_name, presence: true
-        validates :family_name, presence: true
-        validates :first_name_kana, presence: true
-        validates :family_name_kana, presence: true
-        validates :birth_day, presence: true
+  has_many :items, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :buyers
+  validates :nick_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day, presence: true
 end
