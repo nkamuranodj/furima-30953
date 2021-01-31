433bfc6c03943c5b862d1be63517c3ccc0abc2ce
nkamuranodj <nkamuranodj@yahoo.co.jp>
2021-01-31 13:49:26 +0900

    ルボコップ実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nick_name             {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 7)}
+    nick_name             { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 7) }
     password_confirmation { password }
     first_name            { 'テスト' }
     family_name           { 'テスト' }
