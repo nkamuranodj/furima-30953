a14aa814984f650c7bfca680ac5e9f7d8417f45b
nkamuranodj <nkamuranodj@yahoo.co.jp>
2021-01-31 13:46:10 +0900

    コードレビュー後の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :buyers
-  validates :nick_name, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_day, presence: true
+  with_options presence: true do
+  validates :nick_name
+  validates :first_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :family_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day 
   validates :encrypted_password, :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/ }
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nick_name             { 'test' }
-    email                 { 'test@example' }
-    password              { 'a00000' }
+    nick_name             {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 7)}
     password_confirmation { password }
     first_name            { 'テスト' }
     family_name           { 'テスト' }
