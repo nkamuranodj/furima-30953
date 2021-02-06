38ddb670f8b1886c1230f79ecd03f505dade8d44
nkamuranodj <nkamuranodj@yahoo.co.jp>
2021-02-07 02:40:56 +0900

    アクティブストレージの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_one :buyers, dependent: :destroy
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :delively_payer_id
+    validates :ship_area_id
+    validates :ship_day_id
+
+
+  def was_attached?
+    self.image.attached?
+  end
 end
