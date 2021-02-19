e9d5bfbab08313333d5f9e9e09c07f2676a47ad0
nkamuranodj <nkamuranodj@yahoo.co.jp>
2021-02-20 05:58:40 +0900

    スペル修正及び作成順に並ぶ記述

--- db/migrate/20210201072011_create_items.rb
+++ db/migrate/20210201072011_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :category_id, null:false
       t.integer :condition_id, null:false
       t.string :price, null:false
-      t.integer :delively_payer_id, null:false
+      t.integer :delivery_payer_id, null:false
       t.integer :ship_area_id, null:false
       t.integer :ship_day_id, null:false
       t.references :user, foreign_key: true
