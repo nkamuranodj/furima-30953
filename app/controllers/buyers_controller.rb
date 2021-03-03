b5e8e608f7eeb7c7ffd374dd82a2bf9d0292dbc8
nkamuranodj <nkamuranodj@yahoo.co.jp>
2021-03-03 12:24:13 +0900

    buyersコントローラー、ルート作成

---
+++ app/controllers/buyers_controller.rb
@@ -0,0 +1,10 @@
+class BuyersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'buyers/index'
   devise_for :users
   root to: "items#index"
   resources :items do
+  resources :buyers, only: [:new, :create]
   end
 end
